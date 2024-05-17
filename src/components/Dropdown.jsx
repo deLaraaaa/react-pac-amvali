be1dfeb1c79495460d8874b789644d4185f2d762
Rafael de Lara <tecinfo@Amvali.local>
2024-05-17 10:21:37 -0300

    fixed booksvg

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -3,6 +3,8 @@ import TerritorySvg from './assets/TerritorySvg'; // SVG component for 'infra'
 import TreeSvg from './assets/TreeSvg'; // SVG component for 'ambient'
 import TapSvg from './assets/TapSvg'; // SVG component for 'hidro'
 import RiverSvg from './assets/RiverSvg'; // SVG component for 'hidro'
+import BookSvg from './assets/BookSvg'; // SVG component for 'education'
+import Card from './Card';
 
 function Dropdown() {
   const [selected, setSelected] = useState("");
@@ -15,22 +17,26 @@ function Dropdown() {
     infra: TerritorySvg,
     ambient: TreeSvg,
     hidro: TapSvg,
-    river: RiverSvg
+    river: RiverSvg,
+    education: BookSvg
   };
 
   const SvgComponent = SVG_MAP[selected];
 
   return (
-    <div>
-      {SvgComponent && <SvgComponent />}
+    <div >
       <p>Selecione a Categoria do seu Plano</p>
       <select className="dropdown" value={selected} onChange={handleChange}>
         <option value="">Categoria do Plano...</option>
         <option value="infra">Infraestrutura</option>
         <option value="ambient">Meio Ambiente</option>
+        <option value="education">Educação</option>
         <option value="hidro">Hidro</option>
         <option value="river">Rio</option>
       </select>
+      <div className="dropdown-img">
+        {SvgComponent && <SvgComponent />}
+      </div>
     </div>
   );
 }
